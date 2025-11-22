00e85b07542cfd3d22be6e75f4ecbfc962922780
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-11-22 15:43:02 +0100

    Import fix

--- mimisbrunnr-frontend/src/Home.jsx
+++ mimisbrunnr-frontend/src/Home.jsx
@@ -1,8 +1,8 @@
-import SchildSectie from "../../components/Home/SchildSectie/SchildSectie";
-import PlanningSectie from "../../components/Home/PlanningSectie/PlanningSectie";
-import KarakteristiekenSectie from "../../components/Home/KarakteristiekenSectie/KarakteristiekenSectie";
-import FotoSectie from "../../components/Home/FotoSectie/FotoSectie";
-import VisieEnSponSectie from "../../components/Home/VisieEnSponSectie/VisieEnSponSectie";
+import SchildSectie from "./components/Home/SchildSectie/SchildSectie";
+import PlanningSectie from "./components/Home/PlanningSectie/PlanningSectie";
+import KarakteristiekenSectie from "./components/Home/KarakteristiekenSectie/KarakteristiekenSectie";
+import FotoSectie from "./components/Home/FotoSectie/FotoSectie";
+import VisieEnSponSectie from "./components/Home/VisieEnSponSectie/VisieEnSponSectie";
 
 const Home = () => {
   return (
