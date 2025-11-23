e5e6793294abdbdafa45ae34d7c0ed9f24a302e0
Britt Emanuel <britt.emanuel@student.hogent.be>
2025-11-23 15:31:01 +0100

    hero afgewerkt

--- mimisbrunnr-frontend/src/components/Generic/TextLink.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-const TextLink = ({ text, link }) => {
-  return (
-    <Link to={link} className='text-link'>{text}</Link>
-  )
-}
-
-export default TextLink;
--- mimisbrunnr-frontend/src/components/Home/SchildSectie/SchildSectie.jsx
+++ mimisbrunnr-frontend/src/components/Home/SchildSectie/SchildSectie.jsx
@@ -1,13 +1,13 @@
 import SchildInfo from "./SchildInfo/SchildInfo";
 import './SchildSectie.css';
 
-//TODO: Mobile styling
-
 const SchildSectie = () => {
   return (
-    <div className="schild-sectie">
-      <SchildInfo />
-      <img src="./BackgroundImg1.png" alt="Generieke achtergrond foto." height="857px" width="444px" />
+    <div className="dark-bg schild-sectie">
+      <img src="./BackgroundImg1.png" alt="Generieke achtergrond foto." className="bg-image" />
+      <div className="container">
+        <SchildInfo />
+      </div>
     </div>
   )
 }
