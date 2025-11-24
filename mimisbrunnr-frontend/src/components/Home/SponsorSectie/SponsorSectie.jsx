13b0d70c94db675a06a7ed1ba66f38b2c06cbfec
Britt Emanuel <britt.emanuel@student.hogent.be>
2025-11-24 20:21:43 +0100

    fix margin bottom verdween onder events

--- mimisbrunnr-frontend/src/components/Home/SponsorSectie/SponsorSectie.jsx
+++ mimisbrunnr-frontend/src/components/Home/SponsorSectie/SponsorSectie.jsx
@@ -10,7 +10,7 @@ const SponsorSectie = () => {
         <h1 className='sponsor-titel'>Onze Sponsors</h1>
       </div>
       <SponsorList sponsors={sponsors} />
-      <div className='container-sm-bm'>
+      <div className='container-sm-bm sponsor-button'>
         <div className="button-wrapper">
           <PrimaryButton 
             text='Meer info' 
