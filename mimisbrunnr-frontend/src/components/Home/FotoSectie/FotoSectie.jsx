2cedcfeeb5dd0fee50e583928bd01a12a2706e45
Britt Emanuel <britt.emanuel@student.hogent.be>
2025-11-25 19:55:59 +0100

    foto grid home en dynamisch grid alvast voor gallery page

--- mimisbrunnr-frontend/src/components/Home/FotoSectie/FotoSectie.jsx
+++ mimisbrunnr-frontend/src/components/Home/FotoSectie/FotoSectie.jsx
@@ -1,24 +1,32 @@
 import PrimaryButton from '../../Common/PrimaryButton';
-import ImageGrid from '../../Images/ImageGrid';
+import ImageGrid from '../../Afbeeldingen/ImageGrid';
 import './FotoSectie.css';
 
 const FotoSectie = () => {
   const images = [
-    "Groepsfoto4.jpg",
     "Groepsfoto1.jpg",
     "Groepsfoto2.jpg",
     "Groepsfoto3.jpg",
     "Groepsfoto4.jpg",
     "Groepsfoto1.jpg",
     "Groepsfoto2.jpg",
-    "Groepsfoto3.jpg",
-    "Groepsfoto4.jpg"
+    "Groepsfoto3.jpg"
   ];
+
+  const areas = ['a','b','c','d','e','f','g']
 
   return (
     <div className='container' style={{ paddingTop: 5 }}>
       <h1>Enkele sfeerbeelden</h1>
-      <ImageGrid images={images} />
+
+      <div className='fixed-image-grid'>
+        {images.map((src, i) => (
+          <div key={i} className="fixed-image-grid-item" style={{ gridArea: areas[i] }}>
+            <img src={src} alt="" />
+          </div>
+        ))}
+      </div>
+
       <div className="button-wrapper">
         <PrimaryButton
           text='Bekijk alles'
