1bf9e99a3cfc8bb3ac9d67f0a7a552088263cabe
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-11-27 21:00:29 +0100

    Animatietje toegevoegd :)

--- mimisbrunnr-frontend/src/components/Footer/Big Footer/ContactSection/ContactForm/FooterContactForm.jsx
+++ mimisbrunnr-frontend/src/components/Footer/Big Footer/ContactSection/ContactForm/FooterContactForm.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
 import InputField from "../../../../Form/InputField/InputField";
 import SelectField from "../../../../Form/SelectField/SelectField";
-import './ContactForm.css';
+import "./ContactForm.css";
 
 const FooterContactForm = () => {
-
   const onderwerpOpties = ["Algemeen", "Evenement", "Extra info"];
 
   const [naam, setNaam] = useState("");
@@ -32,47 +33,64 @@ const FooterContactForm = () => {
 
   return (
     <div>
-      
-      {submitted && (
-        <div className="form-status success">
-          <i class="fa-regular fa-circle-check"></i>
-          Je bericht werd succesvol verzonden!
-        </div>
-      )}
+      <AnimatePresence>
+        {submitted && (
+          <motion.div
+            key="successMessage"
+            className="form-status success"
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.3 }}
+          >
+            <i className="fa-regular fa-circle-check"></i>
+            Je bericht werd succesvol verzonden!
+          </motion.div>
+        )}
+      </AnimatePresence>
+
       <form onSubmit={handleSubmit}>
-        <InputField 
-          type="text" 
-          label="Naam" 
-          placeholder={""} 
-          value={naam} 
-          onChange={(e) => setNaam(e.target.value)}/>
+        <InputField
+          type="text"
+          label="Naam"
+          value={naam}
+          onChange={(e) => setNaam(e.target.value)}
+        />
 
-        <InputField 
-          type="email" 
-          label="Email" 
-          placeholder={"john.heimdal@placeholder.be"} 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} />
+        <InputField
+          type="email"
+          label="Email"
+          placeholder="john.heimdal@placeholder.be"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <SelectField 
-          label={"Onderwerp"} 
-          placeholder={"Maak een keuze..."} 
-          options={onderwerpOpties} 
-          value={onderwerp} 
-          onChange={(val) => setOnderwerp(val)} />
+        <SelectField
+          label={"Onderwerp"}
+          placeholder={"Maak een keuze..."}
+          options={onderwerpOpties}
+          value={onderwerp}
+          onChange={(val) => setOnderwerp(val)}
+        />
 
-        <InputField 
-          type="message" 
-          label="Bericht" 
-          placeholder={"Vul hier je boodschap in..."} 
-          value={bericht} 
-          onChange={(e) => setBericht(e.target.value)} />
+        <InputField
+          type="message"
+          label="Bericht"
+          placeholder={"Vul hier je boodschap in..."}
+          value={bericht}
+          onChange={(e) => setBericht(e.target.value)}
+        />
 
-        <button type="submit" className="primary-button-dark" disabled={!isFormValid}>Verstuur</button>
+        <button
+          type="submit"
+          className="primary-button-dark"
+          disabled={!isFormValid}
+        >
+          Verstuur
+        </button>
       </form>
-      
     </div>
-  )
-}
+  );
+};
 
 export default FooterContactForm;
