0aae5f32b2d5f14488141abdef496e63fd8e711f
Britt Emanuel <britt.emanuel@student.hogent.be>
2025-11-27 15:25:20 +0100

    start validatie form
    werkt nog niet top

--- mimisbrunnr-frontend/src/components/Footer/Big Footer/ContactSection/ContactForm/FooterContactForm.jsx
+++ mimisbrunnr-frontend/src/components/Footer/Big Footer/ContactSection/ContactForm/FooterContactForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import InputField from "../../../../Form/InputField/InputField";
 import SelectField from "../../../../Form/SelectField/SelectField";
 import './ContactForm.css';
@@ -6,14 +7,28 @@ const FooterContactForm = () => {
 
   const onderwerpOpties = ["Algemeen", "Evenement", "Extra info"];
 
+  const [naam, setNaam] = useState("");
+  const [email, setEmail] = useState("");
+  const [onderwerp, setOnderwerp] = useState("");
+  const [bericht, setBericht] = useState("");
+
+  const emailIsValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const isFormValid = naam !== "" && emailIsValid && onderwerp !== "" && bericht !== "";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isFormValid) return;
+    console.log("Form submitted!");
+  };
+
   return (
     <div>
-      <form>
-        <InputField type="text" label="Naam" placeholder={"John Heimdal"} />
-        <InputField type="email" label="Email" placeholder={"john.heimdal@placeholder.be"} />
-        <SelectField label={"Onderwerp"} placeholder={"Maak een keuze..."} options={onderwerpOpties} />
-        <InputField type="message" label="Bericht" placeholder={"Vul hier je boodschap in..."} />
-        <button type="submit" className="primary-button-dark">Verstuur</button>
+      <form onSubmit={handleSubmit}>
+        <InputField type="text" label="Naam" placeholder={""} value={naam} onChange={(e) => setNaam(e.target.value)}/>
+        <InputField type="email" label="Email" placeholder={"john.heimdal@placeholder.be"} value={email} onChange={(e) => setEmail(e.target.value)} />
+        <SelectField label={"Onderwerp"} placeholder={"Maak een keuze..."} options={onderwerpOpties} value={onderwerp} onChange={(val) => setOnderwerp(val)} />
+        <InputField type="message" label="Bericht" placeholder={"Vul hier je boodschap in..."} value={bericht} onChange={(e) => setBericht(e.target.value)} />
+        <button type="submit" className="primary-button-dark" disabled={!isFormValid}>Verstuur</button>
       </form>
     </div>
   )
