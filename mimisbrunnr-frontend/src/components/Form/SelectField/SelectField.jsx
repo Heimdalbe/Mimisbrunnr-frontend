1cea492e64fdd1446042f9c957be9c178b3d0864
Britt Emanuel <britt.emanuel@student.hogent.be>
2025-11-27 15:14:56 +0100

    eigen dropdown

--- mimisbrunnr-frontend/src/components/Form/SelectField/SelectField.jsx
+++ mimisbrunnr-frontend/src/components/Form/SelectField/SelectField.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import './SelectField.css';
 
 const SelectField = ({ label, placeholder, options }) => {
+  const [selected, setSelected] = useState(placeholder);
+  const [open, setOpen] = useState(false);
+  
   return (
     <div className='select-field'>
-      <label htmlFor={label}>{label}</label>
-      <select>
-        <option selected disabled>{placeholder}</option>
-        {
-          options.map((o) => (
-            <option value={o}>{o}</option>
-          ))
-        }
-      </select>
+      <label>{label}</label>
+
+      <div 
+        className='dropdown'
+        onClick={() => setOpen(!open)}
+      >
+        <div className='dropdown-select'>
+          <span className='select'>{selected}</span>
+          <i className={`fa ${open ? "fa-caret-up" : "fa-caret-down"}`}></i>
+
+        </div>
+
+        <div 
+          className='dropdown-list'
+          style={{
+            opacity: open ? 1 : 0,
+            visibility: open ? "visible" : "hidden"
+          }}
+        >
+          {options.map((o) => (
+              <div 
+                key={o} 
+                className='dropdown-list__item'
+                onClick={() => { setSelected(o); setOpen(false); }}
+              >
+                {o}
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
