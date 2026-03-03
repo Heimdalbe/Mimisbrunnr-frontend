9d055ffad6f29af6ca7306d1335e9d5f14e6d8f1
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-03-03 18:00:51 +0100

    Boekje pagina progress

---
+++ mimisbrunnr-frontend/src/api/boekjes.js
@@ -0,0 +1,19 @@
+const boekjes = [
+  {
+    id: 1,
+    bestand: "/boekje1.pdf",
+    datum: new Date(),
+  },
+  {
+    id: 2,
+    bestand: "/boekje2.pdf",
+    datum: new Date(),
+  },
+  {
+    id: 3,
+    bestand: "/boekje3.pdf",
+    datum: new Date(),
+  },
+];
+
+export default boekjes;
--- mimisbrunnr-frontend/src/pages/Over ons/Boekje/Boekje.jsx
+++ mimisbrunnr-frontend/src/pages/Over ons/Boekje/Boekje.jsx
@@ -1,9 +1,35 @@
+import PrimaryButton from "../../../components/Common/PrimaryButton";
+import PdfViewer from "../../../components/PdfViewer/PdfViewer";
+import "./Boekje.css";
+import boekjes from "../../../api/boekjes";
+
 const Boekje = () => {
   return (
     <div className="container-sm-tm">
-      Boekje
+      <div className="title-button-wrapper">
+        <h1>Boekje</h1>
+        <PrimaryButton
+          text={"Wijzig boekjes"}
+          to={"/over-ons/boekje/edit"}
+          isLight={true}
+        />
+      </div>
+      <div className="select-wrapper">
+        <label htmlFor="jaar">Kies een jaar:</label>
+        <select name="jaar" id="jaar">
+          <option value="placeholder" disabled>
+            bv. 2024 - 2025
+          </option>
+          {boekjes.map((b) => (
+            <option key={b.id} value={b.id}>
+              {b.datum.toLocaleDateString()}
+            </option>
+          ))}
+        </select>
+      </div>
+      <PdfViewer bestand={boekjes[0].bestand} />
     </div>
-  )
-}
+  );
+};
 
 export default Boekje;
