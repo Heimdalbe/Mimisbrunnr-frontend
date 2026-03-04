330a832474b5a4b5f3e6ffd3d466db2fb317eaac
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-03-04 11:21:35 +0100

    Selectfield component gebruiken

--- mimisbrunnr-frontend/src/api/boekjes.js
+++ mimisbrunnr-frontend/src/api/boekjes.js
@@ -2,17 +2,17 @@ const boekjes = [
   {
     id: 1,
     bestand: "/boekje1.pdf",
-    datum: new Date(),
+    datum: new Date("2024-01-01"),
   },
   {
     id: 2,
     bestand: "/boekje2.pdf",
-    datum: new Date(),
+    datum: new Date("2025-01-01"),
   },
   {
     id: 3,
     bestand: "/boekje3.pdf",
-    datum: new Date(),
+    datum: new Date("2026-01-01"),
   },
 ];
 
--- mimisbrunnr-frontend/src/pages/Over ons/Boekje/Boekje.jsx
+++ mimisbrunnr-frontend/src/pages/Over ons/Boekje/Boekje.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import PrimaryButton from "../../../components/Common/PrimaryButton";
 import PdfViewer from "../../../components/PdfViewer/PdfViewer";
 import "./Boekje.css";
 import boekjes from "../../../api/boekjes";
+import SelectField from "../../../components/Form/SelectField/SelectField";
+
+//TODO: Selectfield op full width ziet er hier beetje cursed uit
+
+//TODO: Nut van deze pagina nog is herevalueren want er zijn eigenlijk maar 2 boekjes om te tonen (2 edities 2023-2024),
+// alsook weet ik niet of er effectief nog nieuwe boekjes gaan uitkomen aangezien er voor 2024-2025 geen op de huidige site staat.
 
 const Boekje = () => {
+  const labels = boekjes.map((b) => {
+    const year = b.datum ? b.datum.getFullYear() : new Date().getFullYear();
+    return year;
+  });
+
+  const [selectedLabel, setSelectedLabel] = useState(labels[0] ?? "");
+  const current = boekjes[labels.indexOf(selectedLabel)] || boekjes[0];
+
+  const handleChangeBoekje = (label) => {
+    setSelectedLabel(label);
+  };
+
   return (
     <div className="container-sm-tm">
       <div className="title-button-wrapper">
@@ -14,20 +33,14 @@ const Boekje = () => {
           isLight={true}
         />
       </div>
-      <div className="select-wrapper">
-        <label htmlFor="jaar">Kies een jaar:</label>
-        <select name="jaar" id="jaar">
-          <option value="placeholder" disabled>
-            bv. 2024 - 2025
-          </option>
-          {boekjes.map((b) => (
-            <option key={b.id} value={b.id}>
-              {b.datum.toLocaleDateString()}
-            </option>
-          ))}
-        </select>
-      </div>
-      <PdfViewer bestand={boekjes[0].bestand} />
+      <SelectField
+        label={"Kies een jaar:"}
+        placeholder={"bv. 2025 - 2026"}
+        options={labels}
+        value={selectedLabel}
+        onChange={handleChangeBoekje}
+      />
+      <PdfViewer bestand={current.bestand} />
     </div>
   );
 };
