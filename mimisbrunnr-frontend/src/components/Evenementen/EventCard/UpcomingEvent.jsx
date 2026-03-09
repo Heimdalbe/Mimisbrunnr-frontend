47d74c3b5751e71e225b1fb8072d7383217a9b96
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-03-09 14:37:28 +0100

    Upcoming event image styling improved (still wip)

--- mimisbrunnr-frontend/src/components/Evenementen/EventCard/UpcomingEvent.jsx
+++ mimisbrunnr-frontend/src/components/Evenementen/EventCard/UpcomingEvent.jsx
@@ -1,16 +1,24 @@
-import './UpcomingEvent.css';
-import EventIcon from '../Andere/EventIcon';
-import Countdown from '../Andere/Countdown';
+import "./UpcomingEvent.css";
+import EventIcon from "../Andere/EventIcon";
+import Countdown from "../Andere/Countdown";
 
-const UpcomingEvent = ({ title, location, date, start_time, description, image, type }) => {
+const UpcomingEvent = ({
+  title,
+  location,
+  date,
+  start_time,
+  description,
+  image,
+  type,
+}) => {
   return (
-    <div className='event-wrapper'>
-      <div className='info-section'>
+    <div className="event-wrapper">
+      <div className="info-section">
         <div>
-          <div className='title-and-icon'>
+          <div className="title-and-icon">
             <div>
               <h1>{title}</h1>
-              <h2 className='location'>{location}</h2>
+              <h2 className="location">{location}</h2>
             </div>
             <div>
               <EventIcon category={type} />
@@ -18,16 +26,14 @@ const UpcomingEvent = ({ title, location, date, start_time, description, image, 
           </div>
           <p>
             {description}
-            <a href="#">Lees meer...</a>
+            <a href="#"> Lees meer...</a>
           </p>
         </div>
         <Countdown date={date} />
       </div>
-      <div className='image-section'>
-        <img src={image} alt={"Foto van " + title} className='image' />
-      </div>
+      <img src={image} alt={"Foto van " + title} className="image" />
     </div>
-  )
-}
+  );
+};
 
 export default UpcomingEvent;
