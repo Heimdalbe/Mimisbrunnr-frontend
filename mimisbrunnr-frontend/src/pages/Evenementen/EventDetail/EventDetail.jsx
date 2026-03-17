f065f3a69dd6986d806301a68361473f9be9c116
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-03-17 11:29:18 +0100

    CSS progress

--- mimisbrunnr-frontend/src/pages/Evenementen/EventDetail/EventDetail.jsx
+++ mimisbrunnr-frontend/src/pages/Evenementen/EventDetail/EventDetail.jsx
@@ -12,26 +12,48 @@ const EventDetail = () => {
   return (
     <div className="container-sm-tm">
       {/* TODO: Terug knop of breadcrumbs (of allebei :p) */}
-      <div>
-        <img src={`.${event.image}`} alt={event.title} />
+      <div className="img-card-wrapper">
+        <img className="event-img" src={`.${event.image}`} alt={event.title} />
+        <div className="info-card">
+          <div className="icon-info">
+            <i class="fa-solid fa-user-group"></i>
+            <span>Iedereen welkom</span>
+          </div>
+          <div className="icon-info">
+            <i class="fa-solid fa-calendar-days"></i>
+            <span>{event.date.toLocaleDateString()}</span>
+          </div>
+          <div className="icon-info">
+            <i class="fa-solid fa-clock"></i>
+            <span>{event.start_time}</span>
+          </div>
+          <div className="icon-info">
+            <i class="fa-solid fa-location-dot"></i>
+            <span>{event.location}</span>
+          </div>
+        </div>
       </div>
       <div className="description-section">
         <h1>Beschrijving</h1>
         <p>{event.description}</p>
-        <PrimaryButton
-          isLight={true}
-          text={"Inschrijven"}
-          to={`/evenementen/${id}/inschrijven`}
-        />
+        <div className="button-div">
+          <PrimaryButton
+            isLight={true}
+            text={"Inschrijven"}
+            to={`/evenementen/${id}/inschrijven`}
+          />
+        </div>
       </div>
       <div className="interesting-section">
         <h1>Ook interessant...</h1>
         <EventList events={events} limit={true} />
-        <PrimaryButton
-          isLight={true}
-          text={"Alle evenementen"}
-          to={"/evenementen"}
-        />
+        <div className="button-div">
+          <PrimaryButton
+            isLight={true}
+            text={"Alle evenementen"}
+            to={"/evenementen"}
+          />
+        </div>
       </div>
     </div>
   );
