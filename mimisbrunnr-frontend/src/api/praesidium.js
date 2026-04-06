b522066080dfc444ffdaad45e64a86541d69ae22
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-04-06 13:59:49 +0200

    Quotes toevoegen

--- mimisbrunnr-frontend/src/api/praesidium.js
+++ mimisbrunnr-frontend/src/api/praesidium.js
@@ -4,54 +4,63 @@ const praesidium = [
     name: "Seppe Landtsheer",
     role: "Praeses",
     img: "./seppe.png",
+    quote: "Ik ben een coole praeses",
   },
   {
     id: 2,
     name: "Karsten Depoorter",
     role: "Quaestor",
     img: "./karsten.png",
+    quote: "Ik ben een coole Quaestor",
   },
   {
     id: 3,
     name: "Maxe Adams",
     role: "Secretaris",
     img: "./maxe.png",
+    quote: "Ik ben een coole Secretaris",
   },
   {
     id: 4,
     name: "Ricardo De Blust",
     role: "Cultuur",
     img: "./ricardo.png",
+    quote: "Ik ben een coole Cultuur",
   },
   {
     id: 5,
     name: "Axelle Ducouran",
     role: "Media",
     img: "./axelle.png",
+    quote: "Ik ben een coole Media",
   },
   {
     id: 6,
     name: "Alejandro De Bruyne",
     role: "Sport",
     img: "./alejandro.png",
+    quote: "Ik ben een coole Sport",
   },
   {
     id: 7,
     name: "Richy Rahman",
     role: "Feest & LAN",
     img: "./richy.png",
+    quote: "Ik ben een coole Feest & LAN",
   },
   {
     id: 8,
     name: "Yoran Ollevier",
     role: "ICT",
     img: "./yoran.png",
+    quote: "Ik ben een coole ICT",
   },
   {
     id: 9,
     name: "Zeaya Nys",
     role: "PR",
     img: "./zeaya.png",
+    quote: "Ik ben een coole PR",
   },
 
   {
@@ -59,6 +68,7 @@ const praesidium = [
     name: "Simon Tytgat",
     role: "Schachtentemmer",
     img: "./simon.png",
+    quote: "Ik ben een coole Schachtentemmer",
   },
 ];
 
