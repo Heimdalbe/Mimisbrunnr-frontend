200b8d4d5fd32fcca7d422912aff08f28e07b4e9
Milan Dhondt <milan.dhondt@student.hogent.be>
2026-04-06 14:20:43 +0200

    Label + select items meer in thema gemaakt

--- mimisbrunnr-frontend/src/pages/Praesidium/Praesidium.jsx
+++ mimisbrunnr-frontend/src/pages/Praesidium/Praesidium.jsx
@@ -2,13 +2,13 @@ import SelectField from "../../components/Form/SelectField/SelectField";
 import UserList from "../../components/UserList/UserList";
 import praesidium from "../../api/praesidium";
 
-const options = ["geel", "groen"];
+const options = ["2024-2025", "2025-2026"];
 
 const Praesidium = () => {
   return (
     <div className="container-sm-tm">
       <h1>Praesidium</h1>
-      <SelectField options={options} />
+      <SelectField label={"Kies een jaar:"} options={options} />
       <UserList users={praesidium} />
     </div>
   );
