e20811c8cbcdb49d9583b65ce967a5cf7a0c1b2d
yoran ollevier <yoran.ollevier1@gmail.com>
2026-04-30 11:08:05 +0200

    Finished functionality Admin-Album pages

--- mimisbrunnr-frontend/src/main.jsx
+++ mimisbrunnr-frontend/src/main.jsx
@@ -24,6 +24,7 @@ import AdminHome from "./pages/Admin/Home/AdminHome.jsx";
 import AdminAlbums from "./pages/Admin/Albums/AdminAlbums.jsx";
 import AdminNewAlbum from "./pages/Admin/Albums/AdminNewAlbum.jsx";
 import AdminEditAlbum from "./pages/Admin/Albums/AdminEditAlbum.jsx";
+import AdminAlbumImages from "./pages/Admin/Albums/AdminAlbumImages.jsx";
 
 import NotFound from "./pages/NotFound/NotFound.jsx";
 import Unauthorized from "./pages/NotFound/Unauthorized.jsx";
@@ -81,6 +82,7 @@ const router = createBrowserRouter([
       { path: "/admin/albums", Component: AdminAlbums },
       { path: "/admin/albums/new", Component: AdminNewAlbum },
       { path: "/admin/albums/:id", Component: AdminEditAlbum },
+      { path: "/admin/albums/:id/images", Component: AdminAlbumImages },
       { path: "/admin/*", Component: NotFound },
     ]
   }
