0bddf1d1b0d1f9f8e8f522d36ec376ddecbe6c7d
milandhondt <dhondt.milan@protonmail.com>
2026-08-06 12:14:59 +0200

    laatste linting errors weggewerkt

--- mimisbrunnr-frontend/src/components/Sponsors/SponsorDetails/SponsorDetails.jsx
+++ mimisbrunnr-frontend/src/components/Sponsors/SponsorDetails/SponsorDetails.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 import './SponsorDetails.css';
-import { div } from 'motion/react-client';
 import SponsorOverlay from '../SponsorOverlay/SponsorOverlay';
 
-const SponsorDetails = ({ id, name, logo, website }) => {
+const SponsorDetails = ({ id, name, logo }) => {
   const [showOverlay, setShowOverlay] = useState(false);
 
   // useEffect pagina niet scrollable maken als er een overlay geopend is
@@ -20,11 +19,11 @@ const SponsorDetails = ({ id, name, logo, website }) => {
   }, [showOverlay]);
   return (
     <>
-      <div className='sponsor-wrapper' onClick={() => setShowOverlay(true)}>
-        <div className='sponsor-img-wrapper'>
-          <img className='sponsor-logo' src={logo.url} alt={`Logo van ${name}`} ></img>
+      <div className="sponsor-wrapper" onClick={() => setShowOverlay(true)}>
+        <div className="sponsor-img-wrapper">
+          <img className="sponsor-logo" src={logo.url} alt={`Logo van ${name}`}></img>
         </div>
-        <h2 className='sponsor-name'>{name}</h2>
+        <h2 className="sponsor-name">{name}</h2>
       </div>
       {showOverlay && <SponsorOverlay id={id} setShowOverlay={setShowOverlay} />}
     </>
