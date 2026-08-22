b72f8bafba37bbf327634705484a8691cf165aad
milandhondt <dhondt.milan@protonmail.com>
2026-08-22 21:42:09 +0200

    Tekstjes hardcoden want wie wil da ooit aanpassen met handige knoppen

--- mimisbrunnr-frontend/src/components/Home/KarakteristiekenSectie/KarakteristiekenSectie.jsx
+++ mimisbrunnr-frontend/src/components/Home/KarakteristiekenSectie/KarakteristiekenSectie.jsx
@@ -1,7 +1,33 @@
 import { useState, useEffect } from 'react';
 import KarakteristiekList from './KarakteristiekList/KarakteristiekList';
 import './KarakteristiekenSectie.css';
-import characteristics from '../../../api/characteristics';
+
+const characteristics = [
+  {
+    id: 1,
+    iconClass: 'fa-solid fa-display',
+    name: 'TI HOGENT',
+    text: 'Officiële studentenvereniging van toegepaste informatica aan de HOGENT.',
+  },
+  {
+    id: 2,
+    iconClass: 'fa-solid fa-hand-spock',
+    name: 'GEEKS',
+    text: 'Hello there! These are the people you are looking for.',
+  },
+  {
+    id: 3,
+    iconClass: 'fa-solid fa-gamepad',
+    name: 'GAMERS',
+    text: 'Van een potje Skyrim tot een ronde Mario Kart. Er staat gegarandeerd iemand paraat om te joinen.',
+  },
+  {
+    id: 4,
+    iconClass: 'fa-solid fa-paintbrush',
+    name: 'JAARTHEMA',
+    text: 'Jaarlijks staan onze events in het teken van een nieuw uitgekozen pop culture thema.',
+  },
+];
 
 const KarakteristiekenSectie = () => {
   const images = [
--- mimisbrunnr-frontend/src/components/Home/VisieSectie/VisieSectie.jsx
+++ mimisbrunnr-frontend/src/components/Home/VisieSectie/VisieSectie.jsx
@@ -1,6 +1,20 @@
 import './VisieSectie.css';
 import KernwaardenList from './Kernwaarden/KernwaardenList';
-import kernwaarden from '../../../api/kernwaarden';
+
+const kernwaarden = [
+  {
+    id: 1,
+    waarde: 'Open voor iedereen',
+  },
+  {
+    id: 2,
+    waarde: 'Grote vriendengroep',
+  },
+  {
+    id: 3,
+    waarde: 'Iedereen kan zichzelf zijn',
+  },
+];
 
 const VisieSectie = () => {
   return (
@@ -9,8 +23,12 @@ const VisieSectie = () => {
         <div className="vision-info-section-text">
           <h1>Onze visie</h1>
           <p>
-            Dit is een plaatsvervangend tekstje om wat meer info te geven over de visie van Heimdal die we dus voor ogen
-            hebben. Hieronder komt er ook een opsomming voor de kernwaarden te noteren.
+            Wij zijn Heimdal. De studentenvereniging voor geeks en gamers. 
+            Samen met onze leden willen wij een leuke community creëren voor iedereen met dezelfde interesses. 
+            Dit gaat van een leuke babbel in ons clubcafé de Comic Sans tot een LAN-party in resto D te campus 
+            Schoonmeersen van HOGENT tot een leuke Doop in het Citadelpark. Daarnaast organiseren wij doorheen 
+            het jaar tal van activiteiten om onze leden in de watten te leggen en een fantastische studententijd 
+            te doen beleven.
           </p>
         </div>
         <KernwaardenList kernwaarden={kernwaarden} />
