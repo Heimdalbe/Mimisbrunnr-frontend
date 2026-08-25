029367c9ae16a66268b64a1da677d202a723aa1c
Wout Lesschaeve <wout.lesschaeve@student.hogent.be>
2026-08-25 14:57:21 +0200

    emails en telefoonnrs toegevoegd ipv een form

--- mimisbrunnr-frontend/src/components/Footer/Big Footer/ContactSection/ContactSection.jsx
+++ mimisbrunnr-frontend/src/components/Footer/Big Footer/ContactSection/ContactSection.jsx
@@ -4,7 +4,30 @@ const ContactSection = () => {
   return (
     <div className="contact-section">
       <h1 className="footer-title">Contact</h1>
-      <FooterContactForm />
+      <ul>
+        <li>
+          <b>E-mail</b>
+          <ul>
+            <li>
+              Algemeen: <a href="mailto:gate@heimdal.be">gate@heimdal.be</a>
+            </li>
+            <li>
+              Public Relations: <a href="mailto:pr@heimdal.be">pr@heimdal.be</a>
+            </li>
+            <li>
+              Praeses: <a href="mailto:praeses@heimdal.be">praeses@heimdal.be</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <b>Telefoon</b>
+          <ul>
+            <li>
+              Praeses: <a href="tel:">+32 483 38 50 18</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 };
