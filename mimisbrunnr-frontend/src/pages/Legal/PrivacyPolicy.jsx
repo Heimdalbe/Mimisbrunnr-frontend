bb724e396ac2fbb9ce7b9dfcdf7da0d18612d574
milandhondt <dhondt.milan@protonmail.com>
2026-09-08 14:22:38 +0200

    privacy policy inhoud

--- mimisbrunnr-frontend/src/pages/Legal/PrivacyPolicy.jsx
+++ mimisbrunnr-frontend/src/pages/Legal/PrivacyPolicy.jsx
@@ -1,8 +1,96 @@
 const PrivacyPolicy = () => {
   return (
-    <div className="container-sm-tm">
-      PrivacyPolicy
-    </div>
+    <main className="container-sm-tm">
+      <h1>Privacy Policy</h1>
+
+      <p>
+        Heimdal verwerkt via haar site{' '}
+        <a href="https://www.heimdal.be/">https://www.heimdal.be/</a>{' '}
+        privacygevoelige of persoonsgegevens. Heimdal hecht veel belang aan de
+        privacy van haar partners en besteedt veel aandacht aan de bescherming
+        van de persoonsgegevens. Wij verwerken gegevens in overeenstemming
+        met de vereisten die zijn vastgelegd in de Wet bescherming
+        persoonsgegevens (DPDA). Heimdal is verantwoordelijk voor de
+        gegevensverwerking. In onderstaande privacyverklaring wordt uitgelegd
+        waarom welke persoonsgegevens verzameld worden. We raden u aan dit
+        aandachtig te lezen.
+      </p>
+
+      <h2>Gebruik van persoonsgegevens</h2>
+      <p>
+        Door gebruik te maken van onze service, verstrekt u ons bepaalde
+        gegevens. Dit kunnen persoonlijke gegevens zijn. We bewaren en
+        gebruiken alleen de persoonlijke gegevens die door u rechtstreeks zijn
+        verstrekt of waarvan duidelijk is dat deze aan ons zijn verstrekt om te
+        worden verwerkt. Deze privacyverklaring is in de eerste plaats gericht
+        op degenen die de website gebruiken als particulier of in naam van een
+        vereniging.
+      </p>
+      <p>De gegevens die wij verwerken bij gebruik van onze website en faciliteiten:</p>
+      <ul>
+        <li>Naam, ter identificatie van het lidmaatschap.</li>
+        <li>
+          E-mailadres, om kennis te geven van updates in verband met uw account
+          en u te contacteren omtrent activiteiten.
+        </li>
+        <li>Interessevoorkeuren.</li>
+      </ul>
+
+      <h2>Bewaartermijn</h2>
+      <p>
+        Uw gegevens worden 6 jaar na de intrede van uw lidmaatschap automatisch
+        verwijderd. Indien u vragen heeft over het verwijderen van gegevens kan
+        u ons contacteren via{' '}
+        <a href="mailto:privacy@heimdal.be">privacy@heimdal.be</a>.
+      </p>
+
+      <h2>Onze specifieke doelen</h2>
+      <p>
+        De gegevens, door u aan ons verleend, zullen enkel voor praktische
+        doeleinden gebruikt worden. Er zullen in geen geval gegevens
+        doorgegeven worden aan derden zonder uw toestemming, met uitzondering
+        van de derden hieronder vermeld.
+      </p>
+
+      <h2>Gegevens verstrekken aan derden</h2>
+      <p>
+        Heimdal maakt geen gebruik van derde partijen die toegang hebben tot
+        persoonsgegevens.
+      </p>
+
+      <h2>Beveiliging</h2>
+      <p>
+        We nemen beveiligingsmaatregelen om misbruik van en ongeoorloofde
+        toegang tot persoonlijke gegevens te verminderen. We nemen met name de
+        volgende maatregelen:
+      </p>
+      <ul>
+        <li>
+          Toegang tot eigen persoonlijke gegevens vereist het gebruik van
+          gebruikersnaam en wachtwoord.
+        </li>
+        <li>
+          Toegang tot persoonlijke gegevens gebeurt enkel door de webmaster, die
+          de gegevens behandelt zoals hiervoor beschreven.
+        </li>
+      </ul>
+
+      <h2>Veranderingen aan de privacy policy</h2>
+      <p>
+        We behouden ons het recht voor om deze verklaring te wijzigen. Eventuele
+        wijzigingen zullen terdege aangekondigd worden op onze website.
+      </p>
+      <p><em>De Privacy Policy is laatst gewijzigd op 13/03/2021.</em></p>
+
+      <h2>Inspectie en wijziging van uw gegevens</h2>
+      <p>
+        Mocht u een klacht hebben over de verwerking van uw persoonsgegevens dan
+        vragen wij u hierover direct contact met ons op te nemen. Ook voor
+        vragen of verzoeken over uw privacy en ons privacybeleid kunt u contact
+        met ons opnemen per e-mail via{' '}
+        <a href="mailto:privacy@heimdal.be">privacy@heimdal.be</a>.
+      </p>
+    </main>
   );
 };
 
