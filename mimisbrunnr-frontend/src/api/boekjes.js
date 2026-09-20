5965afb9435eba327eeae6c2dc32a21d5ccfad00
milandhondt <dhondt.milan@protonmail.com>
2026-09-20 09:42:40 +0200

    bestandsnamen ook wat duidelijker gemaakt van de boekjes

--- mimisbrunnr-frontend/src/api/boekjes.js
+++ mimisbrunnr-frontend/src/api/boekjes.js
@@ -1,17 +1,17 @@
 const boekjes = [
   {
     id: 1,
-    bestand: '/boekje1.pdf',
+    bestand: '/Boekje22-23.pdf',
     label: '2022-2023',
   },
   {
     id: 2,
-    bestand: '/boekje2.pdf',
+    bestand: '/Boekje23-24nr1.pdf',
     label: '2023-2024, editie 1',
   },
   {
     id: 3,
-    bestand: '/boekje3.pdf',
+    bestand: '/Boekje23-24nr2.pdf',
     label: '2023-2024, editie 2',
   },
 ];
