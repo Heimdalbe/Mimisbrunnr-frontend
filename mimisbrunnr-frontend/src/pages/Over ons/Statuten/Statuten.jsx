ee42364ba1d4eecf2dfdadf980bfe9cb20b7d58b
milandhondt <dhondt.milan@protonmail.com>
2026-09-20 09:40:40 +0200

    Boekje ipv jaartal gwn label gemaakt voor t selecteren

--- mimisbrunnr-frontend/src/pages/Over ons/Statuten/Statuten.jsx
+++ mimisbrunnr-frontend/src/pages/Over ons/Statuten/Statuten.jsx
@@ -1,6 +1,5 @@
 import PdfViewer from '../../../components/PdfViewer/PdfViewer';
 import statuten from '../../../api/statuten';
-import PrimaryButton from '../../../components/Common/PrimaryButton/PrimaryButton';
 import './Statuten.css';
 
 const Statuten = () => {
@@ -8,13 +7,6 @@ const Statuten = () => {
     <div className="container-sm-tm">
       <div className="title-button-wrapper">
         <h1>Statuten</h1>
-        {/* TODO:  Button verstoppen achter auth 
-        <PrimaryButton
-          text={"Wijzig statuten"}
-          isLight={true}
-          to={"/over-ons/statuten/edit"}
-        />
-        */}
       </div>
       <h2>Laatst aangepast op {statuten.datumAangepast}</h2>
       <PdfViewer bestand={statuten.bestandUrl} />
