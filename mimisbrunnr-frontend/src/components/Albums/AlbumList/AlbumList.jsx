82e03083fc2413b3e5fbf9c3db060c66930f25e8
Wout Lesschaeve <wout.lesschaeve@student.hogent.be>
2026-09-25 14:10:14 +0200

    albums op datum gesorteerd en Alejandro is niet meer dood

--- mimisbrunnr-frontend/src/components/Albums/AlbumList/AlbumList.jsx
+++ mimisbrunnr-frontend/src/components/Albums/AlbumList/AlbumList.jsx
@@ -7,8 +7,9 @@ const AlbumList = ({ albums = [] }) => {
       <h1>Albums</h1>
 
       <div className="album-grid">
-        {
-          albums.map((album) => (
+        {albums
+          .sort((a, b) => new Date(b.date) - new Date(a.date))
+          .map((album) => (
             <AlbumCard
               key={album.id}
               id={album.id}
@@ -17,8 +18,7 @@ const AlbumList = ({ albums = [] }) => {
               coverImage={album.coverImage}
               published={album.published}
             />
-          ))
-        }
+          ))}
       </div>
     </div>
   );
